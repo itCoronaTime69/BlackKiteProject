53bd32287f5110ff245d059fb84a2f65fbdac683
jammie96 <65440492+jammie96@users.noreply.github.com>
2020-05-16 22:31:32 +0800

    updated the usermainpage (user's feed)
    Can continue with our plan

--- app/src/main/java/com/example/inventory/UserMainPage.kt
+++ app/src/main/java/com/example/inventory/UserMainPage.kt
@@ -1,12 +1,29 @@
 package com.example.inventory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_user_main_page.*
 
+//user's feed
 class UserMainPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_main_page)
+
+        gotoInvest.setOnClickListener {
+            startActivity(Intent(this,Main2Activity::class.java)) //to link to next page
+        }
+        gotoReqLoan.setOnClickListener {
+            startActivity(Intent(this,Main2Activity::class.java)) //to link to next page
+        }
+        gotoExp.setOnClickListener {
+            startActivity(Intent(this,Main2Activity::class.java)) //to link to next page
+        }
+        gotoProfile.setOnClickListener {
+            startActivity(Intent(this,Main2Activity::class.java)) //to link to next page
+        }
+
     }
 }
